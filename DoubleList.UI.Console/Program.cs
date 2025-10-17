f8a68f95dd903a84041bcad24230836cd5a737ce
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-10-16 19:29:40 -0500

    Add project files.

---
+++ DoubleList.UI.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using Double_List;
+
+var list = new DoubleLinkedList<string>();
+list.InsertAtBeginning("Perro");
+list.InsertAtBeginning("Gato");
+list.InsertAtBeginning("Loro");
+Console.WriteLine(list.GetForward());
+Console.WriteLine(list.GetBackward());
